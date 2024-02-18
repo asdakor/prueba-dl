af511e00a5eb050a629c47cdcb8d99464125ea7d
Diego <diegofti123@gmail.com>
2024-02-18 05:46:34 -0300

    integrando jquery

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,9 @@
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+
+$('#enviarFormulario').click(function(){
+    var nombre = $('#nombre').val()
+    var asunto = $('#asunto').val()
+    alert('Muchas gracias por contactarnos, '+nombre+'.\n Dentro de unas horas te contactaremos por el asunto: '+asunto)
+})
